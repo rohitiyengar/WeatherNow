290cf656febaab05ed5184099419b36b1c342454
rohitiyengar <rohit.iyengar@gmail.com>
2015-11-07 21:16:40 -0700

    Added Models

---
+++ app/src/main/java/com/project/mobilecomputing/weathernow/models/Location.java
@@ -0,0 +1,64 @@
+package com.project.mobilecomputing.weathernow.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by rohit.iyengar on 11/7/2015.
+ */
+public class Location implements Serializable {
+    private float latitude;
+    private float longitude;
+    private String country;
+    private String city;
+    private long sunset;
+    private long sunrise;
+
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public long getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(long sunset) {
+        this.sunset = sunset;
+    }
+
+    public long getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(long sunrise) {
+        this.sunrise = sunrise;
+    }
+}
